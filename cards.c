74e1cbeb3dcf5c819db1b08d934d59d713cb68a2
jumblies <glamke@gmail.com>
2018-12-20 19:45:47 -0500

    working on pointers

--- cards.c
+++ cards.c
@@ -6,19 +6,34 @@
 */
 #include <stdio.h>
 
+void count_increase(int *cnt){
+    // *cnt = *cnt + 1;
+    (*cnt)++; //Pointers require special methods in order not move the position
+    // it is an order of precedence issue
+    // https://stackoverflow.com/questions/8208021/how-to-increment-a-pointer-address-and-pointers-value
+    printf("Count = %i\n", *cnt);
+}
+
+
 int main()
 {
+
     int count = 0;
-    int reps = 10;
-    printf("count is stored at %p\n\n", &count); //& is the address of operator
-        
+    int reps = 2;
+
     while (reps >= 0)
     {
-        reps --; 
+        reps--;
         char card_name[3];
+
         puts("Enter the card name");
         scanf("%2s", card_name);
         int val = 0;
+
+        printf("count is stored at %p\n", &count); //& is the address of operator
+        printf("value is stored at %p\n", &val);   //& is the address of operator
+        printf("reps is stored at %p\n", &reps);   //& is the address of operator
+        
         if (card_name[0] == 'K' || card_name[0] == 'Q' || card_name[0] == 'J')
         {
             val = 10;
@@ -31,12 +46,12 @@ int main()
         {
             puts("Count has gone up.");
             val = atoi(card_name);
-            count ++; 
+            count_increase(&count);
         }
         else if (val >= 10)
         {
             puts("Count has gone down.");
-            count --;
+            count--;
         }
 
         // {
@@ -44,9 +59,9 @@ int main()
         //     return 1;
         // }
         // printf("There are %i cards\n", (decks * 52));
-        printf("The value is %i \n", (val));
-        printf("The value of the count is %i.\n", count);
-
+        printf("The value is %i and is stored at %p.\n", val, &val);
+        printf("The count is %i and is stored at %p.\n", count, &count);
+        printf("The reps=%i and is stored at %p.\n", reps, &reps);
     }
     return 0;
 }
